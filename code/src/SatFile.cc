59a32f985cb87ea9f1dacd3a0be4b351d0987ee0
Pandeul <pandeul@MacBook-Pro-de-Pandeul.local>
2017-10-29 12:39:16 +0100

    ajout de l'ecriture dans un fchier, modification de la thirdClause qui etait mal codee (on avait mis -x au lieu de x)

--- code/src/SatFile.cc
+++ code/src/SatFile.cc
@@ -1,66 +1,97 @@
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 #include "all.h"
 using namespace std ;
 
-#define v 5
+
+int nbClauses = 0;
+
+int usage(string c)
+{
+  cout << "Usage : " << c << " k (outFile)" << endl;
+  return 1;
+}
 
 static int getVerticeRank(int v, int k) { return v*(k+1) + 1; }
 
-static void firstClause(int k) {
+static string firstClause(int k) {
+  string f = "";
   for(int v = 0 ; v < orderG() ; ++v) {
     for(int i = 0 ; i <= k ; ++i) {
       for(int j = 0 ; j <= k ; ++j) {
         if(i == j)
           continue ;
-        cout << -(getVerticeRank(v, k)+i) << " " << -(getVerticeRank(v, k)+j) << " " << 0 << endl;
+        f += to_string(-(getVerticeRank(v, k)+i)); f += " "; f += to_string(-(getVerticeRank(v, k)+j)); f += " 0\n";
+        nbClauses++;
       }
     }
   }
+  return f;
 }
 
-static void secondClause(int k) {
+static string secondClause(int k) {
+  string f = "";
   for(int v = 0 ; v < orderG() ; ++v) {
     for(int u = 0 ; u < orderG(); ++u) {
       if(u == v)
         continue ;
 
-      cout << -(getVerticeRank(v, k)) << " " << -(getVerticeRank(u, k)) << " " << 0 << endl ;
+      f += to_string(-(getVerticeRank(v, k))); f += " "; f += to_string(-(getVerticeRank(u, k))); f += " 0\n";
+      nbClauses++;
     }
   }
+  return f;
 }
 
-static void thirdClause(int k) {
+static string thirdClause(int k) {
+  string f = "";
   for(int v = 0 ; v < orderG() ; ++v) {
-    cout << -(getVerticeRank(v,k)+k) << " " ;
+    f += to_string((getVerticeRank(v,k)+k)); f+= " " ;
   }
-  cout << 0 << endl ;
+  f += "0\n";
+  nbClauses++;
+  return f;
 }
 
-static void forthClause(int k) {
+static string forthClause(int k) {
+  string f = "";
   for(int v = 0 ; v < orderG(); ++v) {
     for(int u = 0 ; u < orderG(); ++u) {
       if(u == v || !(are_adjacent(u, v)))
         continue ;
       for(int i = 0, j = i+1; i < k; ++i, ++j)
       {
-        cout << -(getVerticeRank(v,k)+j) << " " << (getVerticeRank(u,k)+i) << " " ;
+        f += to_string(-(getVerticeRank(v,k)+j)); f += " "; f += to_string((getVerticeRank(u,k)+i)); f += " " ;
       }
-      cout << 0 << endl;
+      f += "0\n";
+      nbClauses++;
     }
   }
+  return f;
 }
 
 int main(int argc, char const *argv[]) {
-  if(argc != 2) {
-    return 1 ;
+  if(argc != 2)
+  {
+    string arg = argv[0];
+    return usage(arg);
   }
   int k = atoi(argv[1]);
+  string fileName = "test.dimacs";
+  if(argc == 3)
+    fileName = argv[2];
 
-  firstClause(k);
-  secondClause(k);
-  thirdClause(k);
-  forthClause(k);
+  ofstream file(fileName);
+
+  file << firstClause(k);
+  file << secondClause(k);
+  file << thirdClause(k);
+  file << forthClause(k);
+
+
+  file.seekp(ios::beg);
+  file << "p cnf " << orderG()*(k+1) << " " << nbClauses << endl;
 
   return 0;
 }
